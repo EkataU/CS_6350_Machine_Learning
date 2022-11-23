1b6bcdde06056e7bcce1f1f2c6b355ea64a76991
Ekata Mitra <83133600+EkataU@users.noreply.github.com>
2022-11-22 19:50:15 -0700

    Add files via upload

---
+++ SVM/run.sh
@@ -0,0 +1,14 @@
+echo PrimalSVM schedule1
+
+python3 Ques2a_b.py primal_gamma1
+
+echo PrimalSVM schedule2
+python3 Ques2a_b.py primal_gamma2
+
+echo Dual SVM
+
+python3 Ques3abc.py
+
+echo kernel perceptron
+
+python3 Ques3_d.py
